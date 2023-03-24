17ef0a811a0c34b82f9c70cec17d615da1ce6591
BerinaCrncalo <berina.crncalo@stu.ibu.edu.ba>
2023-03-24 17:24:53 +0100

    web

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?
 $servername = "127.0.0.1" ;
 $username = "root" ;
-$password = "80Sarajevo" ;
-$schema = "testSchema" ;
+$password = "databaseproject" ;
+$schema = "lab3_db" ;
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$schema",$username,$password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully" ;
 
-    $stmt = $conn->prepare("SELECT * FROM Students") ;
+    $stmt = $conn->prepare("SELECT * FROM Users") ;
     $stmt->execute();
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC) ;
     print_r($result) ;

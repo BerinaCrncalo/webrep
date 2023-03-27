dbac622ff1303c2bd7fa825e851886fcf02eff4a
BerinaCrncalo <berina.crncalo@stu.ibu.edu.ba>
2023-03-27 22:27:19 +0200

    web

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
-<?
+<?php
 $servername = "127.0.0.1" ;
 $username = "root" ;
-$password = "database" ;
+$password = "" ;
 $schema = "lab3_db" ;
+
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$schema",$username,$password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully" ;
-
+  
     $stmt = $conn->prepare("SELECT * FROM Users") ;
     $stmt->execute();
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC) ;
     print_r($result) ;
+
 
 }catch(PDOException $e){
     echo "Connection failed : " . $e->getMessage();

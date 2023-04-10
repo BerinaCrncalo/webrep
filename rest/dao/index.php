c235e0d296bb665bc6c4b86ac6d8427f0b4abb26
BerinaCrncalo <berina.crncalo@stu.ibu.edu.ba>
2023-04-10 17:53:58 +0200

    web4

---
+++ rest/dao/index.php
@@ -0,0 +1,43 @@
+<?php
+require 'vendor/autoload.php';
+require 'rest/dao/UserDao.class.php';
+
+
+Flight::register('userDao', 'UserDao');
+
+
+// routes will go here
+
+
+Flight::start();
+
+Flight::route('GET /api/users', function(){
+    Flight::json(Flight::userDao()->getUsers());
+});
+
+
+Flight::route('GET /api/users/@id', function($id){
+    Flight::json(Flight::userDao()->getUserById($id));
+});
+
+
+Flight::route('POST /api/users', function(){
+    $data = Flight::request()->data->getData();
+    Flight::json(Flight::userDao()->addUser($data));
+});
+
+
+Flight::route('PUT /api/users/@id', function($id){
+    $data = Flight::request()->data->getData();
+    Flight::userDao()->updateUser($id, $data);
+    Flight::json(Flight::userDao()->getUserById($id));
+});
+
+
+Flight::route('DELETE /api/users/@id', function($id){
+    Flight::userDao()->deleteUser($id);
+});
+
+
+
+?>
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'a126166bd2e5ed388b8cac6a37a6da13ccc658de',
+        'reference' => 'cf7d5dc01605764077b40625cab8ca95352fb64e',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'a126166bd2e5ed388b8cac6a37a6da13ccc658de',
+            'reference' => 'cf7d5dc01605764077b40625cab8ca95352fb64e',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
